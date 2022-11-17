04b337c57355e662716c79e947505dd05bbc2c83
fernando robles <advancexz@gmail.com>
2022-11-17 09:38:56 -0800

    spring initializer project upload

---
+++ src/main/java/com/fernandorobles/costumerBackbone/CostumerBackboneApplication.java
@@ -0,0 +1,13 @@
+package com.fernandorobles.costumerBackbone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CostumerBackboneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CostumerBackboneApplication.class, args);
+	}
+
+}
